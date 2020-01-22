a6ce10438392166f64d1274f157babf19cc66439
Rodrigo Segui <46058568+Rodrigo-Segui@users.noreply.github.com>
2020-01-21 22:56:16 -0300

    commit

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,16 +1,36 @@
-function criaCaixa(){
-    var boxElement = document.createElement('div')
-    boxElement.setAttribute('class', 'box')
-    boxElement.setAttribute('style', 'background-color:#f00')
-    var criarElement = document.querySelector('#app')
-    criarElement.appendChild(boxElement);
+var btnCriar = document.querySelector('#btnCriar')
+btnCriar.style.color = '#fff'
+btnCriar.style.fontWeight = 'bold';
+btnCriar.style.backgroundColor = '#424242';
+btnCriar.style.border = 'solid #ddd';
+btnCriar.style.width = '200px';
+btnCriar.style.height = '40px';
+btnCriar.style.marginLeft = '45%';
 
+function gerarQuadrado() {
 
+    let boxElement = document.createElement("div");
+    boxElement.style.width = '180px';
+    boxElement.style.height = '180px';
+    boxElement.style.margin = '10px';
+    boxElement.style.backgroundColor = '#f00';
+
+//adiciona a classe .box na div criada
+boxElement.classList.add('box');
+
+    document.body.appendChild(boxElement);
 }
 
-//var btnElement = document.querySelector('button.botao')
-       //     btnElement.onclick = function(){
-         //      var boxElement = document.querySelector('.box')
-          //     boxElement.style.width = 100;
-          //     boxElement.style.height = 100;
-             //
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+  color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+ }
+ var newColor = getRandomColor(); // #E943F0
+ boxElement.style.backgroundColor = newColor;
+
+ 
+
