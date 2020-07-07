966f51d58cd33276fb4a9e945d46d216d2e2fdce
Rodrigo-Segui <rodrigo_segui@hotmail.com>
2020-07-07 18:01:14 -0300

    application api github

--- JavaScript ES6/src/main.js
+++ JavaScript ES6/src/main.js
@@ -1,7 +1,9 @@
+import api from './api'
 class App{
     constructor(){
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository');
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -11,22 +13,59 @@ class App{
         this.formEl.onsubmit= event => this.addRespository(event);
     }
 
-    addRespository(event){
+    setloading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id','loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRespository(event){
 
         event.preventDefault();
 
-        this.repositories.push({
-            name:'rocketseat.com.br',
-            description:'Tire a sua ideia do papel e de vida a sua startup',
-            avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v=4',
-            html_url:'https://github.com/rocketseat/rocketseat.com.br',
-        });
-        this.render();
+        const repoInput = this.inputEl.value;
+        if(repoInput.length === 0){
+            return;
+        }
+
+        this.setloading();
+
+        try{
+            const response = await api.get(`/repos/${repoInput}`);
+            console.log(response);
+
+            const { name, description, html_url, owner:{ avatar_url}} = response.data;
+
+
+
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            this.inputEl.value = '';
+            this.render();
+        }catch(err){
+            console.log('repositorio nao existe')
+
+        }
+
+        this.setloading(false);
+
+
     }
     render(){
         this.listEl.innerHTML = '';
 
-        this.repositories.forEach(respo =>{
+        this.repositories.forEach(repo =>{
             let imgEl = document.createElement('img');
             imgEl.setAttribute('src', repo.avatar_url);
 
@@ -34,11 +73,12 @@ class App{
             titleEl.appendChild(document.createTextNode(repo.name));
 
             let descriptionEl = document.createElement('p');
-            descriptionEl.appendChild(document.createTextNode(this.description));
+            descriptionEl.appendChild(document.createTextNode(repo.description));
 
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target','_blanck');
+            linkEl.setAttribute('href', repo.html_url);
             linkEl.appendChild(document.createTextNode('Acessar'));
 
             let listItemEl = document.createElement('li');
